2091dfe9bd57d12b4d7d3c5078f9377ca64ca4a4
Vincent Manuceau <manuceauv@gmail.com>
2022-07-11 15:55:20 +0200

    debug (missing await)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -9,6 +9,12 @@ export class UsersService {
     constructor(@InjectModel('User') private readonly userModel:Model<User>){}
 
     async create(createUserDto:CreateUserDto):Promise<User>{
+        // Do not create an account with same email !
+        if (await this.findOne(createUserDto.email)){
+            throw new BadRequestException () 
+        }
+
+
         const createdUser = new this.userModel(createUserDto);
         return await createdUser.save();
     }
